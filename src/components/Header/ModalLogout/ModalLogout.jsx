46eed4a88dc8b5c7ec0d9c96643be233ed37b36a
Alex Jimenez <alexjimenezada@gmail.com>
2023-01-09 21:24:12 +0100

    Fix error in ModalLogout

--- src/components/Header/ModalLogout/ModalLogout.jsx
+++ src/components/Header/ModalLogout/ModalLogout.jsx
@@ -19,7 +19,6 @@ const ModalLogout = ({ showModalLogout, setShowModalLogout }) => {
         if (!token) {
             dispatch(reset());
             setShowModalLogout(false);
-            navigate("/login");
         }
     }, [token]);
 
@@ -77,7 +76,10 @@ const ModalLogout = ({ showModalLogout, setShowModalLogout }) => {
                         <button
                             type="button"
                             className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80"
-                            onClick={() => dispatch(logout())}
+                            onClick={() => {
+                                dispatch(logout());
+                                navigate("/login");
+                            }}
                         >
                             Yes, I'm sure
                         </button>
